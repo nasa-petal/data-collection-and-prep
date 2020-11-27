204c394aff670868d5e5b8071892ae1462047cac
Jerry Qian <jeq004@ucsd.edu>
2020-11-27 15:19:43 -0800

    fixed cleaning script

--- clean_airtable.py
+++ clean_airtable.py
@@ -1,21 +1,23 @@
 import pandas as pd
 df = pd.read_csv('airtable_papers.csv')
-
+print(df.columns)
 #Dropping all rows without any URL
-df = df[df['URL'].notna()]
+df = df[df['Primary lit site'].notna()]
 
 #Cleaning function
 def clean_multiple_urls(df):
     for index, row in df.iterrows():
         #Retrieving list of URLS, and only operating if it has multiple
-        urls = row['URL'].split(' ')
+        urls = row['Primary lit site'].split(' ')
         if len(urls) >= 2:
             #If it has multiple URLS, drop that row for now
             df = df.drop(index=index)
             #Create new row for each URL with same data in all other columns
             for url in urls:
                 data = [row['DOI'], row['Paper title'], row['Abstract'], row['Journal'], url,
-                    row['Functions'], row['PDF'], row['Press release']]
+                row['Functions Level I'], row['Functions Level II'], row['Functions Level III- NEW'],
+                row['Functions Level III-OLD'], row['Link to press release']
+                ]
                 #Append new rows to dataframe
                 df.append(data)
     return 
