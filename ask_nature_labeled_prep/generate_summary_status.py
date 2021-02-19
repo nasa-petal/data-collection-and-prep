545e9737a41f0035974f9440deda3f87a7350a0e
Herb Schilling <hschilling@nasa.gov>
2021-02-18 19:35:13 -0500

    added script to generate a summary table

---
+++ ask_nature_labeled_prep/generate_summary_status.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+import numpy as np
+
+status_df = pd.read_csv('Colleen_and_Alex_etl_status.csv')
+
+literature_site_groups = status_df.groupby('literature_site')
+status_summary_df = pd.DataFrame(
+    columns=['literature_site', 'num_papers', 'success', 'error', 'no_code', 'no_labels', 'avg_scrape_time'])
+for literature_site, group in literature_site_groups:
+    result_values = group['get_paper_info_result'].value_counts()
+    success = result_values.get("success", 0)
+    error = result_values.get("error", 0)
+    no_code = result_values.get("no_code", 0)
+    no_labels = group.query('num_labels == 0').num_labels.count()
+    num_papers = group.shape[0]
+    avg_scrape_time = group.scrape_time.mean()
+    status_summary_df = status_summary_df.append(
+        {'literature_site': literature_site, 'num_papers': num_papers, 'success': success, 'error': error,
+         'no_code': no_code, 'no_labels': no_labels,'avg_scrape_time': avg_scrape_time}, ignore_index=True)
+status_summary_df = status_summary_df.sort_values(['num_papers'], ascending=[False])
+
+# Summary line at the top
+sums = status_summary_df.sum().rename('total')
+status_summary_df = pd.concat([pd.DataFrame([sums]), status_summary_df[:]]).reset_index(drop = True)
+
+# for some reason the column order gets changed
+cols_to_order = ['literature_site',]
+new_columns = cols_to_order + (status_summary_df.columns.drop(cols_to_order).tolist())
+status_summary_df = status_summary_df[new_columns]
+
+with pd.option_context('display.max_rows', None,
+                       'display.max_columns', None,
+                       'display.max_colwidth', 100):
+    status_summary_df.to_csv('Colleen_and_Alex_etl_summary_status.csv')
+    status_summary_df.to_html('Colleen_and_Alex_etl_summary_status.html')
