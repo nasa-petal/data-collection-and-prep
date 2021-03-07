01b6efac71694836654f13965d3696d33fcd6172
Herb Schilling <hschilling@nasa.gov>
2021-03-07 14:12:17 -0500

    Modified which_journal to return the full url rather than a nickname; added default time_delay method; Fixed up a get_title for the chicago journals

--- ask_nature_labeled_prep/get_paper_info.py
+++ ask_nature_labeled_prep/get_paper_info.py
@@ -1,20 +1,9 @@
+import random
+from urllib.parse import urlparse
+import time
+
 import requests
 from bs4 import BeautifulSoup
-
-from urllib.parse import urlparse
-
-# _prefixes = [
-#     'www.journals', # order is important! Want to look for this first
-#     'www',
-#     'link',
-#     'meetings',
-#     'academic',
-#     'pubs',
-#     'journals',
-#     'epub',
-#     'connection',
-#     'pdfs'
-# ]
 
 _headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
@@ -31,20 +20,7 @@ def which_literature_site(url):
     url_components = urlparse(url)
     netloc = url_components.netloc
 
-    # for prefix in _prefixes:
-    #     if netloc.startswith(prefix):
-    #         # literature_site = netloc[len(prefix)+1:].split('.')[0]
-    #         literature_site = netloc[len(prefix)+1:]
-    #         return literature_site
-    # literature_site = netloc.split('.')[0]
     literature_site = netloc
-
-    # print(literature_site)
-
-    # if 'www' in url:
-    #     publisher = url.split('.')[1]
-    # else:
-    #     publisher = url.split('.')[0].split('//')[1]
 
     return literature_site
 
@@ -102,6 +78,9 @@ class PaperInfo(object):
                 return False
 
         return True
+
+    def time_delay(self):
+        time.sleep(0.5)
 
 class PaperInfoNature(PaperInfo):
     def get_title(self):
@@ -321,7 +300,11 @@ class PaperInfoPLOS(PaperInfo):
 class PaperInfoUChicago(PaperInfo):
     def get_title(self):
         # given self.html, get the title
-        title = self.soup.find(class_='citation__title').text.strip()
+        title_tag = self.soup.find(class_='citation__title')
+        if title_tag:
+            title = title_tag.text.strip()
+        else:
+            title = ''
         return title
 
     def get_doi(self):
@@ -381,6 +364,9 @@ class PaperInfoOUP(PaperInfo):
         except:
             pdf_url = ""
         return pdf_url
+
+    def time_delay(self):
+        time.sleep(random.randint(5, 10))
 
     # def is_open_access(self):
     #     if self.pdf_link is '':
