4d479c369bcce1522593db22e5ebe7adfddb20a7
Herb Schilling <hschilling@nasa.gov>
2021-04-03 18:00:32 -0400

    make use of a .env file in the home directory as the default but allow a cmd line argument to override

--- ask_nature_labeled_prep/retrieve_airtable.py
+++ ask_nature_labeled_prep/retrieve_airtable.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+import argparse
+import os
+import sys
+
 import requests
 import pandas as pd
 
+from dotenv import load_dotenv
 
 def retrieve_airtable_data(table, api_key):
     '''
@@ -36,10 +42,16 @@ def retrieve_airtable_data(table, api_key):
     return df
 #
 if __name__ == "__main__":
-    from dotenv import load_dotenv
-    import os
+    default_path_to_env = Path( Path.home(), '.petal_env')
 
-    load_dotenv('../.env')
+    parser = argparse.ArgumentParser(prog = sys.argv[0],
+                                     description = "get airtable with labeled papers.")
+    parser.add_argument("--env_path", help = "path to .env file containing API keys",
+                        default = default_path_to_env, type = str)
+
+    args = parser.parse_args()
+
+    load_dotenv(args.env_path)
 
     AIRTABLE_API_KEY = os.getenv("AIRTABLE_API_KEY")
     table = 'Colleen%20and%20Alex'
