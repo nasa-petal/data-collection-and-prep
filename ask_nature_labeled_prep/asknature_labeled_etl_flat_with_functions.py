a92ae7453999ab6bc5e8a75220ef71e4301c1263
SOPHIA STILES <sophia6179@gmail.com>
2021-04-05 21:46:55 -0700

    Added code that scrapes science.sciencemag papers

--- ask_nature_labeled_prep/asknature_labeled_etl_flat_with_functions.py
+++ ask_nature_labeled_prep/asknature_labeled_etl_flat_with_functions.py
@@ -133,7 +133,7 @@ if __name__ == "__main__":
     raw_data_check(df)
 
     # df = filter_by_lit_site(df, 'pubmed.ncbi.nlm.nih.gov')
-    df = filter_by_lit_site(df, 'springer')
+    df = filter_by_lit_site(df, 'sciencemag')
 
     # print("filtered data check")
     # raw_data_check(df)
