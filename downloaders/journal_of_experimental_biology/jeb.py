5ab1eff854db4feaf2a78e352658d8f23ab3192f
Paht J <paht.juangphanich@gmail.com>
2021-06-04 15:59:29 -0400

    JEB scraper working
    need to add some features
    - resume feature
    - append to json before clicking on next

--- downloaders/journal_of_experimental_biology/jeb.py
+++ downloaders/journal_of_experimental_biology/jeb.py
@@ -5,46 +5,45 @@
 '''
 from selenium import webdriver
 from bs4      import BeautifulSoup 
-from requests import get
 from pprint import pprint
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementNotInteractableException
+import time
 JEB_LIMIT = 50
 
 
 def jeb_search_download(query):
-
-    def process_section(section):
-        paragraphs = section.find_all('p')
-        return '\n'.join(p.get_text() for p in paragraphs)
-    
-
+    driver = webdriver.Firefox()
+   
     url    = 'https://journals.biologists.com/search-results?page=1&q=' + query
-    result = get(url)
-
-    soup   = BeautifulSoup(result.content, 'html.parser')
+    driver.get(url)    
+    
+    soup   = BeautifulSoup(driver.page_source, 'html.parser')
     articles = []
-    article_links = ['https://jeb.biologists.org' + x.get('href') for x in soup.find_all('a', attrs={'class': 'sri-figure-title'})]
-    i = 0
-    for article_link in article_links:
-        try:
-            if i == JEB_LIMIT:
-                break
-            article_page = BeautifulSoup(get(article_link).content, 'html.parser')
-            category     = article_page.find(attrs={'class' : 'highwire-cite-category'}).get_text()
-            if category == 'Research Article':
+    article_divs = [x for x in soup.find_all('div', attrs={'class': 'sri-title customLink al-title'})]
+    article_links = ['https://journals.biologists.com/' + a.find(lambda tag:tag.name== 'a' and tag.has_attr('href'))['href'] for a in article_divs]
+    next_link = soup.find('a', attrs={'class':'sr-nav-next al-nav-next'})
+    
+    while (next_link is not None):
+        for article_link in article_links:
+            driver.get(article_link)
+            article_page = BeautifulSoup(driver.page_source, 'html.parser')
+            category     = article_page.find(attrs={'class' : 'article-client_type'}).get_text()
+            if category.lower() == 'research article':
                 properties = dict()
                 properties['url']  = article_link
-                properties['title']    = article_page.find(attrs={'class' : 'highwire-cite-title'}).get_text()
-                properties['authors']  = article_page.find(attrs={'class' : 'highwire-cite-authors'}).get_text()
-                article_page = article_page.find(attrs={'class' : 'fulltext-view'})
-                sections = [process_section(section) for section in article_page.find_all(attrs={'class' : 'section'})]
-                properties['abstract'] = sections[0]
-                properties['intro']    = sections[1]
-                properties['methods']  = sections[2]
-                properties['results']  = sections[3]
-                properties['content']  = '\n'.join(sections[1:])
-                i += 1
-        except AttributeError:
-            pass
+                properties['title']    = article_page.find(attrs={'class' : 'wi-article-title article-title-main'}).get_text()
+                author_divs = article_page.find_all(attrs={'class' : 'al-author-name'})
+                author_names = [a.find(lambda tag:tag.name == 'div' and tag.get('class')== ['info-card-name']) for a in author_divs]
+                author_names = [a.get_text().lstrip().rstrip() for a in author_names if a]
+                properties['authors']  = author_names
+                properties['abstract'] = article_page.find(attrs={'class' : 'abstract'}).get_text()
+                articles.append(properties)
+            driver.execute_script("window.history.go(-1)")
+
+        if next_link:
+            driver.find_element_by_link_text("Next").click()
+            next_link = soup.find('a', attrs={'class':'sr-nav-next al-nav-next'})
 
 if __name__ == "__main__":
     jeb_search_download('feline')
