e333253c026f14cce608955628bd223ed275d3ee
dsmith111 <davidcsmith12@gmail.com>
2021-08-04 11:11:58 -0400

    working function mapping

--- utils/taxonomy_converter/convert_labels.py
+++ utils/taxonomy_converter/convert_labels.py
@@ -1,52 +1,62 @@
 def convert_labels(function_map, labels):
 
     # separate each level of labels into its own list to make it easier to understand
-    level_one = function_map[0]
-    level_two = function_map[1]
-    level_three = function_map[2]
+    petal_level_one = function_map[0]
+    petal_level_two = function_map[1]
+    petal_level_three = function_map[2]
+    ask_level_one = function_map[3]
+    ask_level_two = function_map[4]
+    ask_level_three = function_map[5]
 
     # Separate labels into levels
     label_level_one = labels[0]
     label_level_two = labels[1]
     label_level_three = labels[2]
 
-    label_1_index = 0
-    label_2_index = 0
     label_3_index = 0
+    # PeTaL labels 0, 1, 2 | AskNature 3, 4, 5 | Manual Label Required 6
+    result = [set(), set(), set(), set(), set(), set(), False]
 
-    result = [[], [], []]
+       # Look at level 3 label to see if it exists within our function mapping
 
-    for label_index in range(max([len(label_level_one), len(label_level_two), len(label_level_three)])):
+    new_labels = [[], [], [], [], [], []]
+    if (label_3_index < len(label_level_three)):
+        for label in label_level_three:
+            index3 = ask_level_three.index(
+                label) if label in ask_level_three else -1
 
-        if (label_1_index < len(label_level_one)):
-            index1 = level_one.index(
-                label_level_one[label_index]) if label_level_one[label_index] in level_one else -1
-            if (index1 > 0):
-                result[0].append(level_one[index1])
-            else:
-                # result[0].append(label_level_one[label_index])
-                print(label_level_one[label_index])
-                pass
-            label_1_index += 1
-
-        if (label_2_index < len(label_level_two)):
-            index2 = level_two.index(
-                label_level_two[label_index]) if label_level_two[label_index] in level_two else -1
-            if (index2 > 0):
-                result[1].append(level_two[index2])
-            else:
-                # result[1].append(label_level_two[label_index])
-                pass
-            label_2_index += 1
-
-        if (label_3_index < len(label_level_three)):
-            index3 = level_three.index(
-                label_level_three[label_index]) if label_level_three[label_index] in level_three else -1
             if (index3 > 0):
-                result[2].append(level_three[index3])
+                # Check what to do based on PeTaL label
+                temp_label = petal_level_three[index3]
+
+                if temp_label == "keep":
+                    new_labels[2].append(label)
+                    new_labels[1].append(petal_level_two[index3])
+                    new_labels[0].append(petal_level_one[index3])
+                elif temp_label == "raise":
+                    new_labels[1].append(petal_level_two[index3])
+                    new_labels[0].append(petal_level_one[index3])
+                elif temp_label == "delete":
+                    pass
+                elif temp_label == "manual label":
+                    result[-1] = True
+                else:
+                    new_labels[2].append(temp_label)
+                    new_labels[1].append(petal_level_two[index3])
+                    new_labels[0].append(petal_level_one[index3])
             else:
                 # result[2].append(label_level_three[label_index])
                 pass
+            # Always add ask nature labels
+            new_labels[3].append(ask_level_one[index3]) if ask_level_one[index3] != "" else None
+            new_labels[4].append(ask_level_two[index3]) if ask_level_two[index3] != "" else None
+            new_labels[5].append(ask_level_three[index3]) if ask_level_three[index3] != "" else None
             label_3_index += 1
+    result[0].update(new_labels[0])
+    result[1].update(new_labels[1])
+    result[2].update(new_labels[2])
+    result[3].update(new_labels[3])
+    result[4].update(new_labels[4])
+    result[5].update(new_labels[5])
 
-    return result
+    return [list(item) for item in result[:-1]] + [result[-1]]
--- utils/taxonomy_converter/taxonomy_converter.py
+++ utils/taxonomy_converter/taxonomy_converter.py
@@ -16,8 +16,13 @@ def get_args():
 def get_function_map(input_csv_filename):
     # returns function map as a list of 3 lists (lvl 1, lvl 2, and lvl 3 functions)
     df = pd.read_csv(input_csv_filename)
+    df.fillna("", inplace=True)
+    for column in df.columns:
+        df[column] = df[column].str.lower()
+    
     list_of_lists = [df["Level I"].tolist(), df["Level II"].tolist(),
-                     df["Level III"].tolist()]
+                     df["Level III"].tolist(), df["Alevel I"].tolist(), df["Alevel II"].tolist(),
+                     df["Alevel III"].tolist()]
     return list_of_lists
 
 
@@ -26,9 +31,12 @@ def get_labels(input_csv_filename):
     def labels_to_list(label_list):
         new_list = []
 
-        for label in label_list:
-            eval_list = eval(label)
-            new_list.append(eval_list)
+        for label_set in label_list:
+            if pd.isna(label_set):
+                new_list.append([])
+                continue
+            eval_list = eval(label_set)
+            new_list.append([label.lower() for label in eval_list])
 
         return new_list
 
@@ -44,7 +52,7 @@ def get_labels(input_csv_filename):
 
 def prepare_csv(input_csv_filename, function_map_csv):
 
-    multi_level_labels = get_labels.get_labels(input_csv_filename)
+    multi_level_labels = get_labels(input_csv_filename)
     df = pd.read_csv(input_csv_filename)
     function_map = get_function_map(function_map_csv)
 
@@ -55,12 +63,25 @@ def prepare_csv(input_csv_filename, function_map_csv):
                                                           multi_level_labels[1][index],
                                                           multi_level_labels[2][index]
                                                       ])
-
+        
         df.loc[index, "label_level_1"] = str(new_functions[0])
         df.loc[index, "label_level_2"] = str(new_functions[1])
         df.loc[index, "label_level_3"] = str(new_functions[2])
+        df.loc[index, "ask_label_level_1"] = str(new_functions[3])
+        df.loc[index, "ask_label_level_2"] = str(new_functions[4])
+        df.loc[index, "ask_label_level_3"] = str(new_functions[5])
+        df.loc[index, "manual_label"] = new_functions[6]
 
     return df
+
+def separate_manual_labels(full_dataframe):
+    updated_df = full_dataframe[full_dataframe["manual_label"] == False].copy()
+    manual_df = full_dataframe[full_dataframe["manual_label"] == True].copy()
+    if not manual_df.empty:
+        manual_df.drop("manual_label", inplace = True, axis = 1)
+        manual_df.to_csv("papers_to_label.csv", index=False)
+
+    return updated_df
 
 
 if (__name__ == "__main__"):
@@ -68,4 +89,5 @@ if (__name__ == "__main__"):
     args = get_args()
     function_map = "function_map.csv"
     converted_dataframe = prepare_csv(args.input_csv, function_map)
-    converted_dataframe.to_csv(args.output_csv, index=False)
+    final_dataframe = separate_manual_labels(converted_dataframe)
+    final_dataframe.to_csv(args.output_csv, index=False)
